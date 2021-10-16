0ceea20203c91c73aeb4ca56b3f382253060c6d3
Alexander Bagherzadeh <alexanderbagherzadeh@yahoo.com>
2021-10-16 17:12:02 -0400

    added exercise45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args) {
+        //create instance of WordFinder
+        WordFinder wordFinder = new WordFinder();
+        //call method to readStrings
+        wordFinder.readStrings();
+        //call method to write the formatted string
+        wordFinder.writeString();
+    }
+}
---
+++ exercise45/src/main/java/baseline/WordFinder.java
@@ -0,0 +1,29 @@
+package baseline;
+
+public class WordFinder {
+    //Create a string builder to store the modified string.
+    StringBuilder formattedString = new StringBuilder();
+
+
+    public String replaceWord(String oldWord, String newWord) {
+        //used to replace a word in a string and return the new string.
+        return "";
+    }
+
+    public void readStrings() {
+        //this gets each string from the file and determines if a word needs to be replaced
+    }
+
+    public void storeString(String word) {
+        //this stores the string in the StringBuilder to be used later
+    }
+
+    public void writeString() {
+        //this method writes the formatted string to a file
+    }
+
+    public String getOutputFile() {
+        //this method prompts the user for an output file name and then returns the string
+        return "";
+    }
+}
